86d14f67eeb56fe7b972b02a60f3b7feb0f1b478
chiku12345678 <pasidujayasundara2009@gmail.com>
2025-01-01 16:41:52 +0530

    Create support.js

---
+++ plugins /support.js
@@ -0,0 +1,47 @@
+const config = require('../config')
+
+const {cmd , commands} = require('../command')
+
+
+
+cmd({
+
+    pattern: "support",
+
+    desc: "To get the bot informations.",
+
+    react: "⛓",
+
+    category: "main",
+
+    filename: __filename
+
+},
+
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+
+try{
+
+
+
+let about = ` *👋 Hello ${pushname}*
+
+*🧚FRONEXT-MD Support Channels🧚*
+
+*Youtube Channel Link:* https://youtube.com/@avi_nethunter?si=QhKsrEQjEeeoV8xU
+
+*Whatsapp Channel Link:* https://whatsapp.com/channel/0029VaqPVqA1yT2CTlHi0o13
+
+*©𝙵𝚁𝙾𝙽𝙴𝚇𝚃 𝙼𝙳 𝙱𝚈 𝙲𝚈𝙱𝙴𝚁 𝙵𝚁𝙾𝙻𝚈ッ*`
+
+return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption:about},{quoted: mek})
+
+}catch(e){
+
+console.log(e)
+
+reply(`${e}`)
+
+}
+
+})

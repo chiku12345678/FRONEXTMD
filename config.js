10bedcbab3d0cf9f6ac4bc6b297f83622c4c1b1d
chiku12345678 <pasidujayasundara2009@gmail.com>
2025-01-01 17:00:37 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "put sesson id",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/tr1wj5.jpg",
+ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi FRONEXT-MD Is Online Now 💻\n*💻 Owner* - FRONEXT-MD\n\n*💻 Owner Number* -94743491027",
+SUDO_NB: process.env.SUDO_NB || "94743491027",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+MODE: process.env.MODE || "public",
+AUTO_VOICE:"true"
+};
+

63bdaac008f9a4f1a90ee2d9b0d6eb3852cb36b1
Lakshan Damayantha <damayanthalakshan8@gmail.com>
2025-01-01 19:51:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put sesson id",
+SESSION_ID: process.env.SESSION_ID || "2dtQjZrI#0HhU3ASOAp5Y7gXjhm4AbrQjMwUqVmE3brpTM3Aodmw",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/tr1wj5.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi FRONEXT-MD Is Online Now 💻\n*💻 Owner* - FRONEXT-MD\n\n*💻 Owner Number* -94743491027",
 SUDO_NB: process.env.SUDO_NB || "94743491027",
